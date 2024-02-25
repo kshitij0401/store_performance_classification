c9bb0deed06cf4002f39e3b3977e5671c776803e
Kshitij Saxena <80394447+kshitij0401@users.noreply.github.com>
2024-02-25 14:37:52 +0530

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 
 def load_model():
-    with open('/Users/kshitijsaxena/Desktop/ML App/Store Performance prediction/saved_steps.pkl', 'rb') as file:
+    with open('saved_steps.pkl', 'rb') as file:
         data = pickle.load(file)
      
 
